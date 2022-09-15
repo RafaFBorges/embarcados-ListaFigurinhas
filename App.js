b85f56b7a4261bc5300c97c8fb6cb346c566845d
Rafael Fernandes Borges <rafaeelfernandes@hotmail.com>
2022-09-14 23:02:16 -0300

    open camera

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { Camera } from "expo-camera";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, SafeAreaView, View } from "react-native";
 
 export default function App() {
+  const [cameraType, setCameraType] = useState(Camera.Constants.Type.back);
+  const [hasPermission, setHasPermission] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status == "granted");
+    })();
+  }, []);
+
+  if (hasPermission == null) {
+    // tela de espera
+    return <View />;
+  }
+
+  if (hasPermission === false) {
+    // usuaria não permitiu
+    return <Text>Aceite utilizar a camera para utilizar o aplicativo</Text>;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Camera style={{ flex: 1 }} type={cameraType} />
+      <StatusBar style={{ backgroundColor: "#ffffff" }} />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
   },
 });
