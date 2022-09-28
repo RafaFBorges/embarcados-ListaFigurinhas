d7d5ddba29a9d61c6e35aca61541b8843fdcdd79
Rafael Fernandes Borges <rafaeelfernandes@hotmail.com>
2022-09-28 00:39:51 -0300

    server conection

--- src/components/card/PictureModal.js
+++ src/components/card/PictureModal.js
@@ -1,15 +1,29 @@
 import React from "react";
-import { StyleSheet, View, Image } from "react-native";
+import { StyleSheet, FlatList, View, Image } from "react-native";
 import { Feather } from "@expo/vector-icons";
 
 import IconButton from "../button/IconButton";
+import BasicText from "../../text/BasicText";
 
-function PictureModal({ picture, closeModal }) {
+function PictureModal({ picture, closeModal, menssagem }) {
   return (
     <View style={styles.modal}>
       <IconButton style={styles.backButton} action={closeModal}>
         <Feather name="arrow-left" size={24} color="black" />
       </IconButton>
+      <View style={styles.responseContainer}>
+        <FlatList
+          contentContainerStyle={styles.list}
+          data={menssagem}
+          renderItem={({ item, index }) => {
+            return (
+              <BasicText key={index} style={styles.responseText}>
+                {item}
+              </BasicText>
+            );
+          }}
+        />
+      </View>
       <Image style={styles.picture} source={{ uri: picture }} />
     </View>
   );
@@ -33,6 +47,29 @@ const styles = StyleSheet.create({
   },
   picture: {
     flex: 1,
+  },
+  responseContainer: {
+    alignContent: "center",
+    alignSelf: "center",
+    boxSizing: "border-box",
+    padding: 16,
+    backgroundColor: "#FFF",
+    borderRadius: 10,
+    position: "absolute",
+    zIndex: 1,
+    bottom: 16,
+    width: "90%",
+    minHeight: 150,
+    justifyContent: "center",
+  },
+  responseText: {
+    alignSelf: "center",
+  },
+  list: {
+    flexGrow: 1,
+    alignSelf: "center",
+    alignContent: "center",
+    justifyContent: "center",
   },
 });
 
--- src/pages/CameraView.js
+++ src/pages/CameraView.js
@@ -13,16 +13,42 @@ function FigurinhasView() {
   const isFocused = useIsFocused();
   const [picture, setPicture] = useState(null);
   const [isShowModal, setShowModal] = useState(false);
+  const [menssagem, setMenssagem] = useState(["Aguardando resposta..."]);
 
   const navigateToFigurinhas = () => {
     navigation.navigate("Figurinhas");
   };
 
-  const takePicture = async () => {
+  const takePicture = async (setData) => {
     if (camRef) {
       const data = await camRef.current.takePictureAsync();
       setPicture(data.uri);
       setShowModal(true);
+
+      let localUri = data.uri;
+      let filename = localUri.split("/").pop();
+      console.log(`LocalUri: ${localUri}`);
+      console.log(`FileName: ${filename}`);
+      let match = /\.(\w+)$/.exec(filename);
+      let type = match ? `image/${match[1]}` : `image`;
+      console.log(`Type: ${type}`);
+      let formData = new FormData();
+      formData.append("photo", { uri: localUri, name: filename, type });
+      console.log(formData);
+
+      fetch("http://192.168.1.2:8000/imagem", {
+        method: "POST",
+        body: formData,
+        header: {
+          "content-type": "multipart/form-data",
+        },
+      })
+        .then((response) => response.json())
+        .then((json) => {
+          setData(json.faltando);
+          console.log(json);
+        })
+        .catch((err) => console.error("não conseguiu buscar no servidor"));
     }
   };
 
@@ -33,7 +59,10 @@ function FigurinhasView() {
           <IconButton style={styles.userButton} action={navigateToFigurinhas}>
             <Feather name="user" size={24} color="black" />
           </IconButton>
-          <IconButton style={styles.cameraButton} action={takePicture}>
+          <IconButton
+            style={styles.cameraButton}
+            action={() => takePicture(setMenssagem)}
+          >
             <Feather name="camera" size={48} color="black" />
           </IconButton>
         </View>
@@ -42,7 +71,11 @@ function FigurinhasView() {
       {isShowModal && (
         <PictureModal
           picture={picture}
-          closeModal={() => setShowModal(false)}
+          closeModal={() => {
+            setShowModal(false);
+            setMenssagem(["Aguardando resposta..."]);
+          }}
+          menssagem={menssagem}
         />
       )}
     </>
